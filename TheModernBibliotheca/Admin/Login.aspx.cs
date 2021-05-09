bbeb29651e6e00b77819151b0bb09f0f9456857b
JOB LIPAT <lipatjj@live.mcl.edu.ph>
2021-05-10 00:27:01 +0800

    implemented login helper

--- TheModernBibliotheca/Admin/Login.aspx.cs
+++ TheModernBibliotheca/Admin/Login.aspx.cs
@@ -4,20 +4,32 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using TheModernBibliotheca.App_Start.Authentication;
+
 namespace TheModernBibliotheca.Admin
 {
     public partial class Login : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (AuthenticationHelper.GetAdminAuth().IsLoggedIn())
+            {
+                Response.Write(AuthenticationHelper.GetAdminAuth().GetUser().Email);
+            }
         }
 
         protected void SubmitBtn_Click(object sender, EventArgs e)
         {
             if (!Page.IsValid) { return; }
 
-
+            if (AuthenticationHelper.GetAdminAuth().Authenticate(EmailTxt.Text, PasswordTxt.Text))
+            {
+                Response.Write("Authentication Success");
+            }
+            else
+            {
+                Response.Write("Authentication Fail");
+            }
         }
     }
 }
---
+++ TheModernBibliotheca/App_Start/Authentication/AuthenticationHelper.cs
@@ -0,0 +1,85 @@
+﻿using System.Web;
+using TheModernBibliotheca._Code.Model;
+
+namespace TheModernBibliotheca.App_Start.Authentication
+{
+
+    public class AuthenticationHelper
+    {
+        private static AuthenticationHelper BorrowerAuthHelper;
+        private static AuthenticationHelper LibarianAuthHelper;
+        private static AuthenticationHelper AdminAuthHelper;
+
+        public static AuthenticationHelper GetBorrowerAuth()
+        {
+            if (BorrowerAuthHelper == null) {
+                BorrowerAuthHelper = new AuthenticationHelper(Constants.LibraryUser.BORROWER_TYPE);
+            }
+            return BorrowerAuthHelper;
+        }
+
+        public static AuthenticationHelper GetLibrarianAuth()
+        {
+            if (LibarianAuthHelper == null)
+            {
+                LibarianAuthHelper = new AuthenticationHelper(Constants.LibraryUser.LIBRARIAN_TYPE);
+            }
+            return LibarianAuthHelper;
+        }
+
+        public static AuthenticationHelper GetAdminAuth()
+        {
+            if (AdminAuthHelper == null)
+            {
+                AdminAuthHelper = new AuthenticationHelper(Constants.LibraryUser.ADMIN_TYPE);
+            }
+            return AdminAuthHelper;
+        }
+
+        readonly string UserType;
+
+        readonly string SESSION_ID_KEY;
+
+        private AuthenticationHelper(string userType)
+        {
+            UserType = userType;
+            SESSION_ID_KEY = userType + "SESSION_ID_KEY";
+        }
+
+        private string SessionId
+        {
+            get
+            {
+                object sessionId = HttpContext.Current.Session[SESSION_ID_KEY];
+
+                return sessionId == null ? null : (string)sessionId;
+            }
+            set
+            {
+                HttpContext.Current.Session[SESSION_ID_KEY] = value;
+            }
+        }
+
+
+        public LibraryUser GetUser()
+        {
+            return AuthenticationService.GetUser(SessionId);
+        }
+
+        public bool Authenticate(string email, string password)
+        {
+            if (!AuthenticationService.TryAuthenticateUser(email, password, UserType, out string sessionId))
+            {
+                return false;
+            }
+
+            SessionId = sessionId;
+            return true;
+        }
+
+        public bool IsLoggedIn()
+        {
+            return AuthenticationService.IsValidSession(SessionId);
+        }
+    }
+}
