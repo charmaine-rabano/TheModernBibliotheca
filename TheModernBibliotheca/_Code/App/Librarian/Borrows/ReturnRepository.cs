5519a65469649ba6ea776aff1aee372fc1b7c00b
charmaine <charmaine.rabano@gmail.com>
2021-05-12 16:54:37 +0800

    implemented Return Book

---
+++ TheModernBibliotheca/_Code/App/Librarian/Borrows/ReturnRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TheModernBibliotheca._Code.Model;
+
+namespace TheModernBibliotheca._Code.App.Librarian.Borrows
+{
+    public class ReturnRepository
+    {
+        public static IEnumerable<ReturnViewModel> GetBooksToReturn()
+        {
+            using (var context = new TheModernDatabaseEntities())
+            {
+                return context.Borrows.Where(b => b.BorrowState == Constants.Borrow.BORROWED_STATE).Select(b => new ReturnViewModel
+                {
+                    BorrowDate = b.DateBorrowed,
+                    BookName = b.BookInstance.BookInformation.Title,
+                    BorrowerName = b.LibraryUser.FirstName + " " + b.LibraryUser.LastName,
+                    BorrowID = b.BorrowID
+                }).ToList();
+            }
+        }
+
+        public static void ReturnBook(int id)
+        {
+
+        }
+    }
+}
